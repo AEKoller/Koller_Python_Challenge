9762b21f7c699d3317731827dcc4c2d9cc6bd941
AEKoller <andrew@andrewkoller.org>
2023-11-12 21:45:07 -0600

    Part 1 complete!

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ budget_data = os.path.join('Pybank','Resources','budget_data.csv')
 def budget_analysis(budget_data):
     num_of_months = 0
     prof_loss = 0
+    monthly_change = []
+    previous_month_profit_loss = None
 
     with open (budget_data, 'r') as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',')
@@ -26,10 +28,54 @@ def budget_analysis(budget_data):
             #profit losses and gains are specific data, and therefore require a specific reference. In this part of the 
             #function, we are adding every integer to the variable 'prof_loss' from each cell in column 2 that is parsed
             prof_loss += int(row[1]) 
-           
-    return num_of_months, prof_loss
+            #Now I need to create a function that calculates the the change from one month to the next. This requires that I
+            #subtract the cell that comes after the current cell BY the current cell
+            #First, I'll create a variable that identifies the integer that is in the current row, column 2, as the 
+            #current integer 
+            current_month_profit_loss = int(row[1])
+            #Let me explain what is happening in the next 5 lines. We've just initialized current_month_profit and 
+            #established that it is the integer within column B(or 1) and in the current row being parsed.
+            #previous_month_profit_loss was initialized in row 16 as None, meaning that it does not equal anything. if 
+            #previous_month_profit_loss is still equal to nothing when it reaches the if statement below, then it skips to
+            #after the if statement, where the value of previous_month_profit_loss beceomes equal to current_month_profit_loss
+            #this puts us back to the top of the for loop, where the next row is parsed. When we finally get back down to
+            #the if statement, previous_month_profit_loss is no longer none, but still the value assigned to it in the previous
+            #loop. Becasue previous_month_profit_loss is no longer None, we go into the if statement. Here, we initialize change, 
+            #and set it to the sum of current_month_profit_loss - previous_month_profit_loss. This then gets appended to a list
+            #which we will use later to calculate the avaerage.
+            if previous_month_profit_loss is not None:
+                change = current_month_profit_loss - previous_month_profit_loss
+                monthly_change.append(change)
+                
+            previous_month_profit_loss = current_month_profit_loss
+    average_change = sum(monthly_change)/ len(monthly_change)
+    #Now I need to find the greatest change, and I will do that by parsing through the list we created that contains the 
+    #changes: monthly_change
+    greatest_increase = 0
+    greatest_decrease = 0
+    for change in monthly_change:
+        if change > greatest_increase:
+            greatest_increase = change
+        if change < greatest_decrease:
+            greatest_decrease = change
+        
+    return num_of_months, prof_loss, average_change, monthly_change, greatest_increase, greatest_decrease
 
-total_months = budget_analysis(budget_data)
-total_prof_loss = budget_analysis(budget_data)
-print("Total number of months: ", total_months)
-print("Total loss/profit: ", total_prof_loss)
+total_months, total_prof_loss, average_change, monthly_change, greatest_increase, greatest_decrease = budget_analysis(budget_data)
+print("")
+print("Financial Analysis")
+print("")
+print("----------------------------")
+print("")
+print("Total months: ", total_months)
+print("")
+print("Total: ", total_prof_loss)
+print("")
+print("Average change: ", average_change)
+print("")
+print("Greatest increase in profits: ", greatest_increase)
+print("")
+print("Greatest decrease in profits: ", greatest_decrease)
+print("")
+## I used this to double check my answers by cross referencing the returned list to Excel data
+#print("Here's a list of monthly changes: ", monthly_change) 
